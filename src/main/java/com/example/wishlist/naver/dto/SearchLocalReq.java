371e0281378892726674434269ce382e5e5bf30e
sunghana92 <sunghana92@gmail.com>
2022-02-02 16:22:57 +0900

    [feat] Add wishList

---
+++ src/main/java/com/example/wishlist/naver/dto/SearchLocalReq.java
@@ -0,0 +1,27 @@
+package com.example.wishlist.naver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchLocalReq {
+    private String query = "";
+    private int display = 1;
+    private int start = 1;
+    private String sort = "random";
+
+    public MultiValueMap<String, String> toMultiValueMap() {
+        var map = new LinkedMultiValueMap<String, String>();
+        map.add("query", query);
+        map.add("start", String.valueOf(display));
+        map.add("display", String.valueOf(display));
+        map.add("sort", sort);
+
+        return map;
+    }
+}
---
+++ src/main/java/com/example/wishlist/restaurant/service/WishListService.java
@@ -0,0 +1,47 @@
+package com.example.wishlist.restaurant.service;
+
+import com.example.wishlist.naver.NaverClient;
+import com.example.wishlist.naver.dto.SearchImageReq;
+import com.example.wishlist.naver.dto.SearchLocalReq;
+import com.example.wishlist.restaurant.dto.WishListDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WishListService {
+
+    private final NaverClient naverClient;
+
+    public WishListDto search(String query) {
+        //지역 검색
+        var searchLocalReq = new SearchLocalReq();
+        searchLocalReq.setQuery(query);
+
+        var searchLocalRes = naverClient.searchLocal(searchLocalReq);
+        //이미지 검색
+        if (searchLocalRes.getTotal() > 0) {
+            var localItem = searchLocalRes.getItems().stream().findFirst().get();
+            var imageQuery = localItem.getTitle().replaceAll("<[^>]*>","");
+            var searchImageReq = new SearchImageReq();
+            searchImageReq.setQuery(imageQuery);
+
+            var searchImageRes = naverClient.searchImage(searchImageReq);
+            if (searchImageRes.getTotal() > 0) {
+                var imageItem = searchImageRes.getItems().stream().findFirst().get();
+
+                //결과를 리턴
+                var result = new WishListDto();
+                result.setTitle(localItem.getTitle());
+                result.setCategory(localItem.getCategory());
+                result.setAddress(localItem.getAddress());
+                result.setReadAddress(localItem.getRoadAddress());
+                result.setHomePageLink(localItem.getLink());
+                result.setImageLink(imageItem.getLink());
+
+                return result;
+            }
+        }
+        return new WishListDto();
+    }
+}
